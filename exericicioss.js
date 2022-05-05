63901b1dfdbe0d44b9c8a5bffc76e8b451ee1455
Terano <102757870+Danil0666@users.noreply.github.com>
2022-05-05 15:41:00 -0300

    Add files via upload

---
+++ exericicioss.js
@@ -0,0 +1,33 @@
+//1
+function nome (a){
+    console.log(a)
+    return
+}
+
+nome('Danilo')
+
+//2
+function soma (a,b){
+    let res = a+b;
+    console.log(res)
+    return
+}
+
+soma(10,20)
+
+//3
+function multiplicação (c,d){
+    console.log(c*d)
+    return
+}
+
+multiplicação(10,9)
+
+//4
+function média(w,x,y,z){
+    let med = w+x+y+z;
+    console.log((med)/4)
+    return
+}
+
+média(1,2,3,4)
